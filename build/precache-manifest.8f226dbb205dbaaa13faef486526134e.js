e2f49cd2493283080092f932984558250f3cb25a
Alex DeGroat <adegroat@mymail.mines.edu>
2019-06-09 16:34:01 -0600

    Add react build folder

---
+++ build/precache-manifest.8f226dbb205dbaaa13faef486526134e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af76068b9feb250d6b6a398c2091670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c632cf246b326a67c04e",
+    "url": "/static/css/main.4b5e88b7.chunk.css"
+  },
+  {
+    "revision": "2736671d16aa80f3c623",
+    "url": "/static/js/2.72e57ab1.chunk.js"
+  },
+  {
+    "revision": "c632cf246b326a67c04e",
+    "url": "/static/js/main.95937024.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
